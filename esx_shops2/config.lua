2ed6969d64036abde761232b7fadc45fafcc9511
NarekSoNaZ <42683526+NarekSoNaZ@users.noreply.github.com>
2018-08-24 22:53:21 +0200

    Add files via upload

---
+++ esx_shops2/config.lua
@@ -0,0 +1,16 @@
+Config              = {}
+Config.DrawDistance = 100
+Config.Size         = {x = 1.5, y = 1.5, z = 1.5}
+Config.Color        = {r = 0, g = 128, b = 255}
+Config.Type         = 1
+Config.Locale       = 'fr'
+
+Config.Zones = {
+
+	narekshop = {
+		Items = {},
+		Pos = {
+			{x = 248.168106079,   y = -46.583744049072, z = 68.94},
+		}
+	}
+}
